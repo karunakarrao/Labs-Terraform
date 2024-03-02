1152a9c3712d16defa27ecc10e8af7d15028edf9
karna <you@example.com>
2024-03-02 16:16:25 +0530

    random providers useswith different options

--- Local_file_Creation/3. random_pet.tf
+++ Local_file_Creation/3. random_pet.tf
@@ -1,8 +1,13 @@
+# creating a random pet using "random" provider and passing pet details into a file. 
+
 resource "random_pet" "my-pet" {
 }
 
 resource "local_file" "my-pet-details" {
-  filename = "../../tmp/my-pet-details.txt"
-# we are refering the random pet information in the my-pet-details.txt
-  content = "My random pet name is '${random_pet.my-pet.id}'"
+  filename = "/tmp/my-pet-details.txt" 
+  content = "My random pet name is '${random_pet.my-pet.id}'"   
+}
+
+output "pet-id" {
+  value = random_pet.my-pet.id
 }
