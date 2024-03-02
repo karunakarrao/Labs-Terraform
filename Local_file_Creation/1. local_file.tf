f1b63c6097a07054df3b250e33c188fb1c409c10
karna <you@example.com>
2024-03-02 11:37:04 +0530

    modified file path

--- Local_file_Creation/1. local_file.tf
+++ Local_file_Creation/1. local_file.tf
@@ -1,4 +1,7 @@
+
+# We are using "local" provider and creating a local testfile using "local_file" resouce.
+
 resource "local_file" "test-file" {
-  filename = "../../tmp/test-file.txt"
-  content = "This is a test file created "
+  filename = "/tmp/test-file.txt"
+  content  = "This is a test file created "
 }
