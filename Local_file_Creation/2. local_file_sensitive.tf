a41b598d995ce450092404f6852e368b36e8ba0b
karna <you@example.com>
2024-03-02 11:41:16 +0530

    modifled file path & comments

--- Local_file_Creation/2. local_file_sensitive.tf
+++ Local_file_Creation/2. local_file_sensitive.tf
@@ -1,4 +1,6 @@
+# creating sensitive file, using "local" provider.
+
 resource "local_sensitive_file" "test-sensitive-file" {
-  filename = "../../tmp/test-sensitive-file.txt"
-  content = "this file is a sensitive file so the data is not visible in STDOUT logs"
+  filename = "/tmp/test-sensitive-file.txt"
+  content  = "this file is a sensitive file so the data is not visible in STDOUT logs"
 }
