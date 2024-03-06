bd786dc64a2debe7ac8d32a75efab2c590b5f0a9
karna <you@example.com>
2024-03-06 19:39:57 +0530

    added user_data  block toinstall the docker during the instance bootup

--- AWS-Docker-Setup/EC2_Docker_setup.tf
+++ AWS-Docker-Setup/EC2_Docker_setup.tf
@@ -17,14 +17,32 @@ resource "aws_instance" "docker" {
 
   user_data = <<-EOF
     #!/bin/bash
-    sudo apt update -y
-    sudo apt install -y docker
+    sudo yum update -y
+    sudo yum install -y docker
+    sleep 10
     sudo systemctl start docker
     sudo systemctl enable docker
   EOF
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker --version"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"            # Change to your SSH user
+      private_key = file("C:\\Users\\karun\\.ssh\\id_rsa") # Change to the path of your private key
+      host        = self.public_ip
+    }
+  }
+
 }
 resource "aws_key_pair" "docker_key" {
-  key_name = "local"
+  key_name   = "local"
   public_key = file("C:\\Users\\karun\\.ssh\\id_rsa.pub")
 }
 
+output "docker_host_info" {
+  value = aws_instance.docker.public_dns
+}
