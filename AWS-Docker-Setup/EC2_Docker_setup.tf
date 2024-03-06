39ef2b959eb546bd350b2976c689137449704a2e
karna <you@example.com>
2024-03-06 11:51:07 +0530

    added public keys to access docker EC2 instance

--- AWS-Docker-Setup/EC2_Docker_setup.tf
+++ AWS-Docker-Setup/EC2_Docker_setup.tf
@@ -13,6 +13,8 @@ resource "aws_instance" "docker" {
   instance_type = var.docker_ami["instance_type"]
   tags          = { Name = "Docker_Host" }
 
+  key_name = aws_key_pair.docker_key.key_name
+
   user_data = <<-EOF
     #!/bin/bash
     sudo apt update -y
@@ -20,5 +22,9 @@ resource "aws_instance" "docker" {
     sudo systemctl start docker
     sudo systemctl enable docker
   EOF
+}
+resource "aws_key_pair" "docker_key" {
+  key_name = "local"
+  public_key = file("C:\\Users\\karun\\.ssh\\id_rsa.pub")
 }
 
